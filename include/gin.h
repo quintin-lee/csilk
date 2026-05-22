1b5e2c7ecfd9e79903b347e32096186ab34c0967
quintin <2449164582@qq.com>
2026-05-22 20:53:11 +0800

    feat: initial project setup and CMake configuration

---
+++ include/gin.h
@@ -0,0 +1,6 @@
+#ifndef GIN_H
+#define GIN_H
+
+#define GIN_VERSION "0.1.0"
+
+#endif
---
+++ tests/test_main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "gin.h"
+
+int main() {
+    printf("Tests running...\n");
+    return 0;
+}
