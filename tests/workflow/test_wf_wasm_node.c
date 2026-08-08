50e0e55571d09c7581c72e7f97a21c3d93e8da6c
quintin <2449164582@qq.com>
2026-08-08 20:05:01 +0800

    fix(test): 🐛 replace nullptr with NULL in test_wf_wasm_node.c

--- tests/workflow/test_wf_wasm_node.c
+++ tests/workflow/test_wf_wasm_node.c
@@ -11,14 +11,14 @@ test_workflow_wasm_node_execution(void)
 {
     const char* dummy_wasm = "test_wf_wasm.wasm";
     FILE*       f = fopen(dummy_wasm, "wb");
-    assert(f != nullptr);
+    assert(f != NULL);
 
     uint8_t header[8] = {0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00};
     fwrite(header, 1, 8, f);
     fclose(f);
 
     csilk_wf_t* wf = csilk_wf_new("wasm_pipeline");
-    assert(wf != nullptr);
+    assert(wf != NULL);
 
     int res = csilk_wf_add_wasm_node(wf, "wasm_step_1", dummy_wasm);
     assert(res == 0);
